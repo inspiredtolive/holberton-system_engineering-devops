794dd89b873112a331dcb3386f1730a8aa7735b2
Marco Chan <inspiredtolive0@gmail.com>
2020-02-06 22:53:49 -0800

    Gets all hot listings of a subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines recurse()."""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """Gets all hot listings of a subreddit."""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) \
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 \
+        Safari/537.36'
+    }
+    params = {'after': None, 'limit': 100}
+    if hot_list:
+        params['after'] = hot_list[-1].get('data').get('name')
+    res = requests.get(
+        url, headers=headers, params=params, allow_redirects=False
+    )
+    if res.status_code == 200:
+        if res.json()['data']['children']:
+            hot_list.extend(res.json()['data']['children'])
+            return recurse(subreddit, hot_list)
+        return hot_list
+    else:
+        print(None)
